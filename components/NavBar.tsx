6316301dfb142f29fd4c938d4baae89c1528fcbc
Alberto Del Buono Paolini <alberto.delbuonopaolini@stud.unifi.it>
2022-09-13 19:17:58 +0200

    Fixed navbar jumping

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,20 +1,26 @@
 import Link from "next/link";
 import React from "react";
+import ThemeSwitch from "./ThemeSwitch";
 
 const NavBar: React.FC = () => {
   return (
-    <nav>
-      <Link href="/" scroll={false}>
-        <a className="text-fgLight dark:text-fgDark pr-6 underline underline-and-vertical-align">
-          Home
-        </a>
-      </Link>
-      <Link href="/about" scroll={false}>
-        <a className="text-fgLight dark:text-fgDark px-6 underline underline-and-vertical-align">
-          About
-        </a>
-      </Link>
-    </nav>
+    <header className="sticky top-0 bg-light dark:bg-dark border-b mb-8 border-fgLight dark:border-fgDark px-12 md:pl-16 lg:px-20 xl:px-28 mx-auto">
+      <div className="flex items-center justify-between py-5">
+        <nav>
+          <Link href="/" scroll={false}>
+            <a className="text-fgLight dark:text-fgDark pr-6 underline underline-and-vertical-align">
+              Home
+            </a>
+          </Link>
+          <Link href="/about" scroll={false}>
+            <a className="text-fgLight dark:text-fgDark px-6 underline underline-and-vertical-align">
+              About
+            </a>
+          </Link>
+        </nav>
+        <ThemeSwitch />
+      </div>
+    </header>
   );
 };
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,19 +3,11 @@ import React from "react";
 import "../styles/globals.css";
 import { AnimatePresence } from "framer-motion";
 import NavBar from "../components/NavBar";
-import ThemeSwitch from "../components/ThemeSwitch";
 
 const MyApp = ({ Component, pageProps, router }) => {
   return (
     <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
-      <header className="sticky top-0 bg-light dark:bg-dark border-b mb-8 border-fgLight dark:border-fgDark">
-        <div className="max-w-[90vw] px-8 mx-auto">
-          <div className="flex items-center justify-between py-5">
-            <NavBar />
-            <ThemeSwitch />
-          </div>
-        </div>
-      </header>
+      <NavBar />
       <AnimatePresence
         exitBeforeEnter
         initial={false}
